a188b0d3be89f166eba9b8cb47ce65d4bb9dfc2d
Kinnedy <taylorkinnedy@gmail.com>
2025-05-22 20:02:16 -0500

    copied and added files to repository

--- Develop/src/interfaces/Candidate.interface.tsx
+++ Develop/src/interfaces/Candidate.interface.tsx
@@ -1,1 +1,28 @@
 // TODO: Create an interface for the Candidate objects returned by the API
+interface GithubUserPreview {
+  login: string;
+  id: number;
+  avatar_url: string;
+  html_url: string;
+  type: string;
+  [key: string]: any; // optional: in case GitHub adds more fields
+}
+
+interface GithubUser {
+  login: string;
+  id: number;
+  avatar_url: string;
+  html_url: string;
+  name: string | null;
+  company: string | null;
+  blog: string | null;
+  location: string | null;
+  email: string | null;
+  bio: string | null;
+  public_repos: number;
+  followers: number;
+  following: number;
+  created_at: string;
+  updated_at: string;
+  [key: string]: any; // optional flexibility
+}
